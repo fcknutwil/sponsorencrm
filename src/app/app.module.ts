6be4a0505f9bcbe922053fe0350da10ca42b33cd
Marcel Arnold <mail@maesi.org>
2016-10-15 17:24:27 +0200

    add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ApiService } from './shared';
@@ -15,7 +17,8 @@ import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
     BrowserModule,
     HttpModule,
     FormsModule,
-    routing
+    routing,
+    NgbModule
   ],
   declarations: [
     AppComponent,
--- src/vendor.ts
+++ src/vendor.ts
@@ -7,7 +7,9 @@ import '@angular/http';
 import '@angular/router';
 
 import 'rxjs';
-import '@angularclass/hmr';
+import '@angularclass/hmr;
+
+import '@ng-bootstrap/ng-bootstrap';
 
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
